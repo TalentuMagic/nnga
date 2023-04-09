de364f84bdc285ed2b00455cd19ae3a6e4928680
TalentuMagic <davidioan2001@gmail.com>
2023-04-09 17:27:27 +0300

    removed unnecessary files

--- HW1/fertility.py
+++
@@ -1,7 +0,0 @@
-from tensorflow import keras
-import matplotlib
-import numpy
-from sklearn import linear_model as lm
-import pandas
-
-dataSet = pandas.read_csv("./DataSets/fertility_Diagnosis.txt", sep=",")
